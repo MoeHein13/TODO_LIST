bc128e6eb5cfa4434cea7f5310442cad7adb68f3
Kelvin <kelvinmoe96@gmail.com>
2025-09-24 23:55:03 +0800

    update the searchbar

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,5 +1,13 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="min-h-dvh flex items-center justify-center">
+      <div className="flex flex-col w-auto gap-2">
+        <h1 className="font-semibold text-2xl text-gray-200 mb-3">
+          Task Manager
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
