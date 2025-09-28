ce641bea32e907dc215159848c513381f039b18f
Kelvin <kelvinmoe96@gmail.com>
2025-09-28 12:18:29 +0800

    add the final step

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,23 +1,27 @@
 import type { ChangeEvent, FormEvent } from "react";
-
+import { CircleCheckBig } from "lucide-react";
 type barType = {
-  tasks: string[];
+  tasks: string;
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
   handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
 };
 
 const SearchBar = ({ tasks, handleChange, handleSubmit }: barType) => {
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className="bg-blue-800 p-3 border-gray-600 rounded  ">
+      <form onSubmit={handleSubmit} className="w-auto flex">
         <input
           type="text"
           placeholder="Create a task"
           value={tasks}
           onChange={handleChange}
+          className="outline-0 text-white  w-full "
         />
+        <button type="submit">
+          <CircleCheckBig color="#f2f2f2" />
+        </button>
       </form>
-    </>
+    </div>
   );
 };
 
